4f17b51aac8e1e8b18462306284a168fdf5b28e2
Arpit Sarang <arpit.01.sarang.2005@gmail.com>
2026-01-22 15:21:44 +0530

    fix the render

--- tekron-frontend/constants/config.ts
+++ tekron-frontend/constants/config.ts
@@ -1,8 +1,10 @@
 import { Platform } from 'react-native';
 
-export const BACKEND_URL = 'http://10.254.200.254:3000';
+export const BACKEND_URL = 'https://tekron-2-0-apk.onrender.com';
 
 export const SOCKET_URL = BACKEND_URL;
 console.log('Configured SOCKET_URL:', SOCKET_URL);
 // Replace with your computer's local IP address
 // You can find this in the Metro bundler logs (e.g., exp://192.168.1.5:8081 -> use http://192.168.1.5:3000)
+
+// CodeMaverick backend URL = http://10.254.200.254:3000
